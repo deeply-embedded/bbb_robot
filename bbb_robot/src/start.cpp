8eb27c67221d114342f15d29448b140d66ee11d8
abp816 <abp816@LAB27>
2016-06-10 09:48:29 +0200

    upload

--- bbb_robot/src/start.cpp
+++ bbb_robot/src/start.cpp
@@ -7,9 +7,36 @@
 //============================================================================
 
 #include <iostream>
+#include <GPIO.h>
+#include "periphery/actor/servo.h"
+
 using namespace std;
+using namespace RSL;
+//using namespace RSL_core;
+
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	double position = 0;
+
+	GPIOPin gpioPin = P8_3;
+
+
+	RSL_core::GPIO::exportPin(gpioPin);
+
+	Servo myServo = new Servo(gpioPin);
+	myServo.enableServo();
+
+	myServo.setPosition(position);
+
+
+	while(1){
+
+
+	}
+
+
+
 	return 0;
 }
